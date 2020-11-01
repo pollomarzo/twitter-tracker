c90e6c5c4dd10a8864a24a49d2c9250c96cb71ca
Matteo Lorenzoni <mtt.lorenzoni@gmail.com>
2020-11-01 19:43:52 +0100

    fix css textfileds and set new alert graphic

--- pages/components/main.js
+++ pages/components/main.js
@@ -6,7 +6,12 @@ import React, { useState } from "react";
 import axios from "axios";
 import Fade from "@material-ui/core/Fade";
 import CircularProgress from "@material-ui/core/CircularProgress";
-
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import { NoSsr } from '@material-ui/core';
 
 const useStyles = makeStyles(() => ({
   button: {
@@ -38,7 +43,10 @@ const useStyles = makeStyles(() => ({
     "&:hover": {
       backgroundColor: "lightblue",
       color: "#1DA1F2",
-   },
+   }
+  },
+  dialog: {
+    color: "#1DA1F2", 
   }
 }));
 
@@ -53,124 +61,158 @@ export default function Main() {
     longitudeEnd: 0,
   });
   const [loading, setLoading] = useState(false);                      //to change button semantic
-  const [id, setId] = useState(0);                                    //to send id of axios/post to axios/delete
+  const [id, setId] = useState(0);                              //to send id of axios/post to axios/delete
+  const [open, setOpen] = useState(false);
 
 
   function handleChange(e) {
     const value = e.target.value;
-    setCoordinates({
-      ...coordinates,
-      [e.target.name]: value,
-    });
+    setCoordinates({...coordinates, [e.target.name]: value });
   }
 
-  const handleClickQuery = () => {
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleClickQuery = () => {
     if (loading) {
+      setLoading(prev => !prev);
+      axios
+      .delete("/api/geoFilter", {
+          data: { id: id },
+          headers: { "Authorization": "***" }
+      })
+      .then((response) => {
+          console.log(response)
+          var dataStr = `data:text/json;charset=utf-8,${encodeURIComponent(JSON.stringify(response.data))}`
+          var dlAnchorElem = document.getElementById("downloadAnchorElem");
+          dlAnchorElem.setAttribute("href", dataStr);
+          dlAnchorElem.setAttribute("download", `${id}.json`);
+          dlAnchorElem.click();
+      })
+      .catch((error) => console.log("Error message: ", error.message));
+    } else {
+      var correctInput = true;
+      const regex = RegExp("^-?[1]?[0-8]?[0-9][.][0-9]{2}$");
+      const input = [coordinates.longitudeStart, coordinates.latitudeStart, coordinates.longitudeEnd, coordinates.latitudeEnd]
+      input.forEach((element) =>{
+          if (!(regex.test(element))) correctInput = false
+      })
+      if (correctInput){
         setLoading(prev => !prev);
         axios
-        .delete("/api/geoFilter", {
-            data: { id: id },
-            headers: { "Authorization": "***" }
+        .post("/api/geoFilter", {
+        coordinates: `${coordinates.longitudeStart},${coordinates.latitudeStart},${coordinates.longitudeEnd},${coordinates.latitudeEnd}`
         })
         .then((response) => {
-            console.log(response)
-            var dataStr = `data:text/json;charset=utf-8,${encodeURIComponent(JSON.stringify(response.data))}`
-            var dlAnchorElem = document.getElementById("downloadAnchorElem");
-            dlAnchorElem.setAttribute("href", dataStr);
-            dlAnchorElem.setAttribute("download", `${id}.json`);
-            dlAnchorElem.click();
+        setId(response.data);
         })
-        .catch((error) => console.log("Error message: ", error.message));
-    } else {
-        var correctInput = true;
-        const regex = RegExp("^-?[1]?[0-8]?[0-9][.][0-9]{2}$");
-        const input = [coordinates.longitudeStart, coordinates.latitudeStart, coordinates.longitudeEnd, coordinates.latitudeEnd]
-        input.forEach((element) =>{
-            if (!(regex.test(element))) correctInput = false
-        })
-        if (correctInput){
-            setLoading(prev => !prev);
-            axios
-            .post("/api/geoFilter", {
-            coordinates: `${coordinates.longitudeStart},${coordinates.latitudeStart},${coordinates.longitudeEnd},${coordinates.latitudeEnd}`
-            })
-            .then((response) => {
-            setId(response.data);
-            })
-            .catch((error) => {
-            console.log(error);
-            });
-        } else {
-            alert("Input error, an acceptable input is a number in range [-180.00, 180.00] written with this formula")
-        }
-        }
+        .catch((error) => {
+        console.log(error);
+        });
+      } else {
+        handleClickOpen()
+      }
+    }
   };
 
   const aStyle = {display: "none"};
 
   return (
-    <div className={styles.main}>
-        <header className={styles.title}>
-            <h1 className={styles.titleH1}>TWITTER TRACKER</h1>
-        </header>
-        <a href="" id="downloadAnchorElem" style={aStyle}></a>
-        <div className={styles.form}>
-            <TextField
-                id="longitudeStart"
-                className={classes.textField}
-                label="Longitude start"
-                variant="outlined"
-                name="longitudeStart"
-                required
-                onChange={handleChange}
-            />
-            <TextField
-                id="latitudeStart"
-                className={classes.textField}
-                label="Latitude start"
-                variant="outlined"
-                name="latitudeStart"
-                required
-                onChange={handleChange}
-            />
-            <TextField
-                id="longitudeEnd"
-                className={classes.textField}
-                label="Longitude end"
-                variant="outlined"
-                name="longitudeEnd"
-                required
-                onChange={handleChange}
-            />
-            <TextField
-                id="latitudeEnd"
-                className={classes.textField}
-                label="Latitude end"
-                variant="outlined"
-                name="latitudeEnd"
-                required
-                onChange={handleChange}
-            />
-            <div className={styles.submit}>
-                <Button
-                    onClick={handleClickQuery}
-                    variant="contained"
-                    className={classes.submitButton}
-                    color="default"
-                    >
-                    {loading ? "STOP" : "START"}
-                </Button>
-                <div>
-                    <Fade
-                        in={loading}
-                        unmountOnExit
-                    >
-                        <CircularProgress />
-                    </Fade>
-                </div>
-            </div>
-        </div>
-    </div>
+    <NoSsr>
+      <div className={styles.main}>
+          <header className={styles.title}>
+              <h1 className={styles.titleH1}>TWITTER TRACKER</h1>
+          </header>
+          <a href="" id="downloadAnchorElem" style={aStyle}></a>
+          <div className={styles.form}>
+              <TextField
+                  id="longitudeStart"
+                  className={classes.textField}
+                  label="Longitude start"
+                  variant="outlined"
+                  name="longitudeStart"
+                  required
+                  onChange={handleChange}
+              />
+              <TextField
+                  id="latitudeStart"
+                  className={classes.textField}
+                  label="Latitude start"
+                  variant="outlined"
+                  name="latitudeStart"
+                  required
+                  onChange={handleChange}
+              />
+              <TextField
+                  id="longitudeEnd"
+                  className={classes.textField}
+                  label="Longitude end"
+                  variant="outlined"
+                  name="longitudeEnd"
+                  required
+                  onChange={handleChange}
+              />
+              <TextField
+                  id="latitudeEnd"
+                  className={classes.textField}
+                  label="Latitude end"
+                  variant="outlined"
+                  name="latitudeEnd"
+                  required
+                  onChange={handleChange}
+              />
+              <div className={styles.submit}>
+                  <Button
+                      onClick={handleClickQuery}
+                      variant="contained"
+                      className={classes.submitButton}
+                      color="default"
+                      >
+                      {loading ? "STOP" : "START"}
+                  </Button>
+                  <div>
+                      <Fade
+                          in={loading}
+                          unmountOnExit
+                      >
+                          <CircularProgress />
+                      </Fade>
+                  </div>
+              </div>
+              <AlertWindow value={open} function={handleClose}/>
+          </div>
+      </div>
+    </NoSsr>
   );
+}
+
+
+
+
+function AlertWindow(props) {
+  const classes = useStyles();
+  
+  return(
+    <Dialog
+    open={props.value}
+    onClose={props.function}
+    >
+    <DialogTitle id="alert-dialog-title" className={classes.dialog}>Bad input</DialogTitle>
+    <DialogContent>
+      <DialogContentText id="alert-dialog-description">
+        An acceptable input is a number in range [-180.00, 180.00] written with this formula
+      </DialogContentText>
+    </DialogContent>
+    <DialogActions>
+      <Button onClick={props.function} variant="contained" className={classes.submitButton} color="default" autoFocus>
+        OK
+      </Button>
+    </DialogActions>
+    </Dialog>
+  )
 }
