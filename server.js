3378decb28e61de74aa13e0f4a311301abaddcba
notchla <liso.lorenzo@gmail.com>
2020-11-17 18:19:33 +0100

    socket first push

---
+++ server.js
@@ -0,0 +1,31 @@
+const { createServer } = require("http");
+const { parse } = require("url");
+const next = require("next");
+const socketIo = require("socket.io");
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const geoStream = require("./twitterAPI/geoStream");
+
+app.prepare().then(() => {
+  const server = createServer((req, res) => {
+    // Be sure to pass `true` as the second argument to `url.parse`.
+    // This tells it to parse the query portion of the URL.
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  });
+
+  const io = socketIo(server);
+
+  io.on("connection", (socket) => {
+    console.log("a user connected");
+    geoStream(socket);
+  });
+
+  server.listen(3000, (err) => {
+    if (err) throw err;
+    console.log("> Ready on http://localhost:3000");
+  });
+});
