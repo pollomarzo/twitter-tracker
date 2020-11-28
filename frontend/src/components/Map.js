465a45cbf640783981313d68824592a50bb916b1
Mattia Guazzaloca <mattia.guazzaloca@gmail.com>
2020-11-28 16:24:46 +0100

    Added tweet grouping by coordinates. This fixes overlapping but we have to tweak threshold

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,5 +1,5 @@
 import React, { useMemo } from 'react';
-import L from 'leaflet';
+import leaflet from 'leaflet';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 import 'leaflet/dist/leaflet.css';
@@ -7,7 +7,7 @@ import markerImg from '../assets/twitter.svg';
 
 const defaultPosition = [44.494704, 11.342005];
 
-const customMarker = new L.Icon({
+const customMarker = new leaflet.Icon({
   iconUrl: markerImg,
   iconRetinaUrl: markerImg,
   iconAnchor: null,
@@ -15,38 +15,73 @@ const customMarker = new L.Icon({
   shadowUrl: null,
   shadowSize: null,
   shadowAnchor: null,
-  iconSize: new L.Point(30, 30),
+  iconSize: new leaflet.Point(30, 30),
   className: 'leaflet-div-icon',
 });
 
-const Map = ({ tweetsList }) => {
-  const markerList = useMemo(
-    () =>
-      tweetsList.map((tweet, index) => {
-        let markerCoords = undefined;
+const THRESHOLD = 0.000001;
 
-        // Accurate coordinates (a point)
-        if (tweet.coordinates && tweet.coordinates.type === 'Point')
-          markerCoords = [...tweet.coordinates.coordinates];
-        // Approximated coordinates (a square/rectangle)
-        else if (tweet.geo != null && tweet.place && tweet.place != null) {
-          // Coordinates vector is structured like this [[[BL, TL, TR, BR]]]
-          const BL = tweet.place.bounding_box.coordinates[0][0];
-          const TR = tweet.place.bounding_box.coordinates[0][2];
-          markerCoords = [(BL[0] + TR[0]) / 2, (BL[1] + TR[1]) / 2];
-        }
-        // The response from Twitter are inverted against the format requested by the library
-        if(markerCoords){
-        return (
-          <Marker position={markerCoords.reverse()} key={index} icon={customMarker}>
-            <Popup>
-              <b>{tweet.user.name}</b>
-              <br />
-              <p>{tweet.text}</p>
-            </Popup>
-          </Marker>
-        );
-      }}),
+const normalizeList = (tweets) =>
+  tweets
+    .map((tweet) => {
+      // Accurate coordinates (a point)
+      if (tweet.coordinates && tweet.coordinates.type === 'Point') {
+        return {
+          username: tweet.user.name,
+          text: tweet.text,
+          coordinates: tweet.coordinates.coordinates,
+        };
+      }
+
+      // Approximated coordinates (a square/rectangle)
+      if (tweet.place && tweet.place.bounding_box) {
+        // Coordinates vector is structured like this [[[BL, TL, TR, BR]]]
+        const bottomLeftCorner = tweet.place.bounding_box.coordinates[0][0];
+        const topRightCorner = tweet.place.bounding_box.coordinates[0][2];
+        return {
+          username: tweet.user.name,
+          text: tweet.text,
+          coordinates: [
+            (bottomLeftCorner[0] + topRightCorner[0]) / 2,
+            (bottomLeftCorner[1] + topRightCorner[1]) / 2,
+          ],
+        };
+      }
+
+      return null;
+    })
+    .filter(Boolean)
+    .reduce((groups, tweet) => {
+      const keys = Object.keys(groups).map((coords) => coords.split(',').map(Number));
+      const key = keys.find(
+        (coords) => Math.abs(coords[0] - tweet.coordinates[0]) <= THRESHOLD
+      );
+
+      if (key) {
+        groups[key].push(tweet);
+      } else {
+        groups[tweet.coordinates.join(',')] = [tweet];
+      }
+
+      return groups;
+    }, {});
+
+const Map = ({ tweetsList }) => {
+  const markers = useMemo(
+    () =>
+      Object.entries(normalizeList(tweetsList)).map(([coords, tweets], index) => (
+        <Marker position={coords.split(',').reverse()} key={index} icon={customMarker}>
+          <Popup>
+            {tweets.map((tweet) => (
+              <>
+                <b>{tweet.username}</b>
+                <br />
+                <p>{tweet.text}</p>
+              </>
+            ))}
+          </Popup>
+        </Marker>
+      )),
     [tweetsList]
   );
 
@@ -56,7 +91,7 @@ const Map = ({ tweetsList }) => {
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {markerList}
+      {markers}
     </MapContainer>
   );
 };
