badc723d31951d257293ee187305a933d8a56c33
Enea <enea.guidi@studio.unibo.it>
2020-12-09 22:04:19 +0100

    Probably fixed mail notification, to be checked

--- frontend/src/components/NotifySettings.js
+++ frontend/src/components/NotifySettings.js
@@ -8,19 +8,20 @@ import { NOTIFICATION } from '../constants';
 const NotifySettings = ({ count }) => {
   const [email, setEmail] = useState(undefined);
   const [treshold, setTreshold] = useState(100);
+  const [mailSent, setSent] = useState(false);
 
   useEffect(() => {
-    if (email && treshold && count >= treshold) {
+    if (!mailSent && email && treshold && count >= treshold) {
+      setSent(true);
       axios
         .put(NOTIFICATION, { address: email, count: count })
         .catch((err) => console.log(err));
     }
-  }, [email, treshold, count]);
+  }, [mailSent, email, treshold, count]);
 
   return (
     <>
       <div className="inputForm">
-        <Typography>User</Typography>
         <InputField label="e-mail" handler={(event) => setEmail(event.target.value)} />
         <Typography>{`Already got ${count} tweets`}</Typography>
       </div>
@@ -28,7 +29,10 @@ const NotifySettings = ({ count }) => {
         <Typography gutterBottom>Treshold</Typography>
         <Slider
           value={treshold}
-          onChange={(_, newT) => setTreshold(newT)}
+          onChange={(_, newT) => {
+            setTreshold(newT)
+            setSent(false);
+          }}
           valueLabelDisplay="auto"
           min={0}
           max={1000}
