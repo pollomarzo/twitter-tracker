589b9d23c4cc0279dcf34e00824240c6005bac86
Enea Guidi <guidienea@gmail.com>
2021-01-13 16:56:06 +0100

    Fixed compilation warning

--- frontend/src/components/GraphSnippets/index.js
+++ frontend/src/components/GraphSnippets/index.js
@@ -1,6 +1,6 @@
 import { default as AreaChart } from './AreaChart';
 import { default as BarChart } from './BarChart';
-import { default as PieChart } from './PieChart';
+import { default as PieChart } from './PieGraph';
 import { default as TableChart } from './TableChart';
 
 export { AreaChart, BarChart, PieChart, TableChart };
