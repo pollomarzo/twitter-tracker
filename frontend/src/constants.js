236f9363bee25b7320aaf12e4c9a64fe9fe7cd75
Paolo Marzolo <pollo.marzo@gmail.com>
2021-01-13 16:50:49 +0100

    fixed typo in fabsdesc for schedule

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -19,8 +19,8 @@ export const FABsDesc = {
   filter: `Here you can select specific filter to be applied to the list currently on the client`,
 
   schedule: `By authenticating here you can set an interval upon wich a tweet will be published on your
-    account with the given text and an image of the map and/or the picture odf the wordcloud`,
+    account with the given text and an image of the map and/or the picture of the wordcloud`,
 
   email: `Here you can set a reminder for when a certain treshold (in number of tweet captured) is surpassed
-    once the treshold is reached an email to the given address will be sent, remember to check the spam`,
+    once the treshold is reached an email to the given address will be sent, remember to check the spam folder`,
 };
